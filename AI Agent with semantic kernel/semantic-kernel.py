eb6e1a53f58b8ca2981d2f517a737c1831130ce9
Yassir Hiroli <168097964+yasssir04@users.noreply.github.com>
2025-06-09 17:35:38 +0300

    Update semantic-kernel.py

--- AI Agent with semantic kernel/semantic-kernel.py
+++ AI Agent with semantic kernel/semantic-kernel.py
@@ -3,7 +3,11 @@ import asyncio
 from pathlib import Path
 
 # Add references
-
+from dotenv import load_dotenv
+from azure.identity.aio import DefaultAzureCredential
+from semantic_kernel.agents import AzureAIAgent, AzureAIAgentSettings, AzureAIAgentThread
+from semantic_kernel.functions import kernel_function
+from typing import Annotated
 
 
 async def main():
@@ -25,23 +29,69 @@ async def main():
 async def process_expenses_data(prompt, expenses_data):
 
     # Get configuration settings
+    load_dotenv()
+    ai_agent_settings = AzureAIAgentSettings()
 
 
     # Connect to the Azure AI Foundry project
+    async with (
+         DefaultAzureCredential(
+             exclude_environment_credential=True,
+             exclude_managed_identity_credential=True) as creds,
+         AzureAIAgent.create_client(
+             credential=creds
+         ) as project_client,
+    ):
 
         
         # Define an Azure AI agent that sends an expense claim email
-
+        expenses_agent_def = await project_client.agents.create_agent(
+             model= ai_agent_settings.model_deployment_name,
+             name="expenses_agent",
+             instructions="""You are an AI assistant for expense claim submission.
+                             When a user submits expenses data and requests an expense claim, use the plug-in function to send an email to expenses@contoso.com with the subject 'Expense Claim`and a body that contains itemized expenses with a total.
+                             Then confirm to the user that you've done so."""
+        )
 
         # Create a semantic kernel agent
+        expenses_agent = AzureAIAgent(
+             client=project_client,
+             definition=expenses_agent_def,
+             plugins=[EmailPlugin()]
+        )
 
 
         # Use the agent to process the expenses data
+        thread: AzureAIAgentThread = AzureAIAgentThread(client=project_client)
+        try:
+             # Add the input prompt to a list of messages to be submitted
+             prompt_messages = [f"{prompt}: {expenses_data}"]
+             # Invoke the agent for the specified thread with the messages
+             response = await expenses_agent.get_response(thread_id=thread.id, messages=prompt_messages)
+             # Display the response
+             print(f"\n# {response.name}:\n{response}")
+        except Exception as e:
+             # Something went wrong
+             print (e)
+        finally:
+             # Cleanup: Delete the thread and agent
+             await thread.delete() if thread else None
+             await project_client.agents.delete_agent(expenses_agent.id)
 
 
 
 # Create a Plugin for the email functionality
-
+class EmailPlugin:
+    """A Plugin to simulate email functionality."""
+    
+    @kernel_function(description="Sends an email.")
+    def send_email(self,
+                   to: Annotated[str, "Who to send the email to"],
+                   subject: Annotated[str, "The subject of the email."],
+                   body: Annotated[str, "The text body of the email."]):
+        print("\nTo:", to)
+        print("Subject:", subject)
+        print(body, "\n")
 
 
 
